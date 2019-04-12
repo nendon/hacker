fd3bd9628d096905b12d674f196cc8c19ce21047
nendon <nenden.lina@gmail.com>
2019-04-12 15:24:25 +0700

    perubahan hardcode landingpage

--- resources/views/web/bootcamp/bootcamp.blade.php
+++ resources/views/web/bootcamp/bootcamp.blade.php
@@ -297,9 +297,7 @@
           </div>
         </div>
         
-        @include('web.bootcamp.testimoni')
-
-        <!-- <div class="row testimoni">
+        <div class="row testimoni">
           <div class="col-xs-12 text-center">
             <h2 class="title text-center">Testimoni dari Siswa</h2>
           </div>
@@ -308,45 +306,57 @@
 
             <div class="slick mx-auto"  style="max-width: 800px;">
               <div>
-                <div class="box">
+                <div class="box" style="height: 300px;">
+             
                   <p>
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    "Alhamdulillaah...saya mendapat banyak ilmu disini. Mudah2an saya bisa lebih percaya diri mengajar karena banyak kosakata baru di dunia IT."
                   </p>
                   
                   <div class="name">
-                    <img src="{{asset('template/bootcamp/img/users.png')}}" class="w-25 pull-left mr-4" alt="">
-                    Rizki Alif irfany <br>
-                    Google Expert
+                    <img src="{{ url('/assets/source/images/testimoni/fitrih.png') }}" class="w-25 pull-left mr-4" alt="">
+                    Fitri Handayani <br>
+                    Guru SMK
                   </div>
                 </div>
               </div>
 
               <div>
-                <div class="box">
+                <div class="box" style="height: 300px;">
                   <p>
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    "Materi sangat bagus dan bisa didownload. Jadi bisa belajar kapanpun dirumah. Saya pun bisa minta dipandu step by stepnya oleh trainernya. Jadi ngga takut gagal" 
                   </p>
                   
                   <div class="name">
-                    <img src="{{asset('template/bootcamp/img/users.png')}}" class="w-25 pull-left mr-4" alt="">
-                    Rizki Alif irfany <br>
-                    Google Expert
+                    <img src="{{ url('/assets/source/images/testimoni/ludy.png') }}" class="w-25 pull-left mr-4" alt="">
+                    Ludy<br>
+                    Sysadmin
                   </div>
                 </div>
               </div>
 
               <div>
-                <div class="box image" style="background-image: url({{asset('template/bootcamp/asset/4.jpg')}});">
+                <!-- <div class="box image" style="background-image: url({{asset('template/bootcamp/asset/4.jpg')}});">
                   <div style="bottom: 8%;position:absolute;color:#fff;">
                     <b>Rizki Alif Irfany</b>
                     <br>
                     <i>Google Expert</i>
                   </div>
-                </div>   
+                </div>    -->
+                <div class="box" style="height: 300px;">
+                  <p>
+                    "Pengajar bersertifikasi dan berpengalaman, nanya-nanya sama trainernya pun malem juga tetep dijawab. mantap. Saya belajar banyak ilmu baru disini yang berguna menunjang pekerjaan saya." 
+                  </p>
+                  
+                  <div class="name">
+                    <img src="{{ url('/assets/source/images/testimoni/ekas.png') }}" class="w-25 pull-left mr-4" alt="">
+                    Eka Saeful<br>
+                    Trainer Mikrotik
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div> -->
+        </div>
 
         <div class="row question">
           <div class="col-xs-12 text-center mb-5">
@@ -666,5 +676,4 @@
     @endif
 
   </script>
-  @include('web.bootcamp.testimoni')
 @endsection
