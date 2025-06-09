60e213926bcc9270de7e8a16157c332527a28df4
Muhammad Fakhri Abdillah <fakhriabdillah37@gmail.com>
2025-06-09 21:01:08 +0700

    Fix Docker errors

--- dockerapp-presetup.sh
+++ dockerapp-presetup.sh
@@ -2,3 +2,6 @@
 
 docker run --rm -v .:/app composer:2.2  config --no-plugins allow-plugins.kylekatarnls/update-helper false
 docker run --rm -v .:/app composer install --ignore-platform-reqs
+
+mkdir -p storage/framework/{sessions,views,cache}
+chmod -R 775 storage/framework
